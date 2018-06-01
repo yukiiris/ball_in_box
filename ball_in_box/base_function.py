b6b4d9c0837d2abecf3170df367393a4c0e04176
chiellini <18319277690@163.com>
2018-06-01 13:09:52 +0800

    脑阔疼

--- ball_in_box/base_function.py
+++ ball_in_box/base_function.py
@@ -1,20 +1,24 @@
-
+import numpy as np
+# the range of X-axis and Y-axis
+XRANGE = (-1, 1)
+YRANGE = (-1, 1)
 
 class Nodexy:
-    x=float(0)
-    y=float(0)
+    x=np.float64(0)
+    y=np.float64(0)
     def __init__(self, x, y):
         self.x=x
         self.y=y
 
 class Nodexyr:
-    x=float(0)
-    y=float(0)
-    r=float(0)
+    x=np.float64(0)
+    y=np.float64(0)
+    r=np.float64(0)
     def __init__(self, x, y,r):
         self.x=x
         self.y=y
         self.r=r
+
 
 def xyobject_to_Nodexy(nodes):
     list_Nodexy=[]
@@ -45,19 +49,58 @@ def Nodexyr_to_xyrobject(nodes):
     print(list_)
     return list_
 
-def find_proper_one_in_xyrobject(_judge,x_or_y,nodes):
+def find_proper_one_in_xyrobject_by_r(_judge,nodes):
     """
     _judge 如果judges为真 则返回最大值 反之返回最小值
-    x_or_y 如果为真，则根据object中x大小排序，反之根据y大小排序
     nodes  需要处理的list,list里面存的是(x,y,r)这样的object
 
     return 返回数字 即是第几个
     """
+    
     m=len(nodes)
-    max_nodexyr=Nodexyr(0,0,0)
+    j=0
+    list_Nodexyr=xyrobject_to_Nodexyr(nodes)
+    if(_judge):
+        max_nodexyr=Nodexyr(0,0,0)
+        for i in range(m):
+            if(list_Nodexyr[i].r>max_nodexyr.r):
+                j=i
+                max_nodexyr=list_Nodexyr[i]
+        return j
+    else:
+        min_nodexyr=Nodexyr(0,0,0)
+        for i in range(m):
+            if(list_Nodexyr[i].r<min_nodexyr.r):
+                j=i
+                min_nodexyr=list_Nodexyr[i]
+        return j
 
+def three_lines_suit_circle(cross1,cross2):
+    r=np.float64(0)
+    x=np.float64(0)
+    y=np.float64(0)
+    if(cross1.x==cross2.x):
+        r=abs(np.float64(cross1.y-cross2.y))
+        y=np.float64(cross1.y+cross2.y)/np.float64(2)
+        x=cross1.x+r
+        if(x<XRANGE[0] or x>XRANGE[1]):
+            x=cross1.x-r
+    else:
+        r=abs(np.float64(cross1.x-cross2.x))
+        x=np.float64(cross1.x+cross2.x)/np.float64(2)
+        y=cross1.y+r
+        if(y<YRANGE[0] or y>YRANGE[1]):
+            y=cross1.y-r
 
+    circle=Nodexyr(x,y,r)
+    return circle
 
+def two_lines_one_node_suit_circle(cross1,node1):
+    delta_x=np.float64(0)
+    delta_y=np.float64(0)
+    # if(cross1.x>0):
+    #     if(cross1.y>0):
+            
 
 def sort_nodes_by_littletobig_xtoy(nodes):
     """
@@ -88,10 +131,10 @@ def judge_node_with_box(nodes,m):
     #有m个点，有m-1条线段，故循环m-1次
     for i_1 in range(m):
         if(i_1==m-1):
-            k=float(nodes[i_1][1]-nodes[0][1])/float(nodes[i_1][0]-nodes[0][1])
+            k=np.float64(nodes[i_1][1]-nodes[0][1])/np.float64(nodes[i_1][0]-nodes[0][1])
             lines.append((nodes[i_1][0],nodes[i_1][1],nodes[0][0],nodes[0][1]))
         else:
-            k=float(nodes[i_1+1][1]-nodes[i_1][1])/float(nodes[i_1+1][0],nodes[i_1][0])
+            k=np.float64(nodes[i_1+1][1]-nodes[i_1][1])/np.float64(nodes[i_1+1][0],nodes[i_1][0])
             lines.append(
                     (nodes[i_1][0],nodes[i_1][1],nodes[i_1+1][0],nodes[i_1+1][1],k)
                     )
